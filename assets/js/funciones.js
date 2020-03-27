4f8713d52c5c8be0af5df4e0dc4551945cef78f7
cesar <bazancesar12@gmail.com>
2020-03-27 17:01:16 -0600

    nuevos cambnios

---
+++ assets/js/funciones.js
@@ -0,0 +1,10 @@
+/*$(document).ready(function() {
+    $(".do-nicescrol").niceScroll({
+    
+    cursorcolor:"#4f0423",
+    cursorwidth:"1px",
+    background:"rgba(20,20,20,0.3)",
+    cursorborder:"1px solid #4f0423",
+    cursorborderradius:0
+    });  // a world full of color! 
+    });*/  
